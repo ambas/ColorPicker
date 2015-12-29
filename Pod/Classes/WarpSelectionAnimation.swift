34ca9ea6ae9c3a38b7936b26b35bab2e19fd4700
Ambas Chobsanti <Ambas@Ambass-MacBook-Pro.local>
2015-12-29 23:10:34 +0700

    Fix ring color

--- Pod/Classes/WarpSelectionAnimation.swift
+++ Pod/Classes/WarpSelectionAnimation.swift
@@ -10,14 +10,14 @@ class WarpSelectionAnimation: ColorPickerSelection {
     
     var selectedRingView: UIView = {
         let view = UIView()
-        view.layer.borderColor = UIColor.colorWithHexString("39B0BA").CGColor
+        view.layer.borderColor = UIColor.colorWithHexString("39B0B8").CGColor
         view.layer.borderWidth = 2
         return view
     }()
     
     var tempRingView: UIView =  {
         let view = UIView()
-        view.layer.borderColor = UIColor.colorWithHexString("39B0BA").CGColor
+        view.layer.borderColor = UIColor.colorWithHexString("39B0B8").CGColor
         view.layer.borderWidth = 2
         return view
     }()
@@ -30,7 +30,7 @@ class WarpSelectionAnimation: ColorPickerSelection {
         dispatch_after(delayTime, dispatch_get_main_queue()) {
             self.selectedRingView = self.tempRingView
             self.tempRingView = UIView()
-            self.tempRingView.layer.borderColor = UIColor.greenColor().CGColor
+            self.tempRingView.layer.borderColor = UIColor.colorWithHexString("39B0B8").CGColor
             self.tempRingView.layer.borderWidth = 2
         }
     }
